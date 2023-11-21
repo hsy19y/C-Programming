21cdeabcb84dc6d538baac7a5d57afe90c6491e5
hsy19y <118896805+hsy19y@users.noreply.github.com>
2023-11-21 17:48:46 +0900

    Add files via upload

---
+++ 20231031/Ring Buffer/Ring buffer.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define BUFFER_SIZE 5
+
+int main() {
+    int ringBuffer[BUFFER_SIZE] = {0,  };
+    int index = 0;
+
+    while (1) {
+        int value;
+        printf("값을 입력 : ");
+        scanf("%d", &value);
+
+        if (value == 0) {
+            printf("ERROR");
+            break;
+        }
+
+        ringBuffer[index] = value;
+
+        // 버퍼 값을 출력
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            printf("%d ", ringBuffer[i]);
+        }
+        printf("\n");
+
+        index = (index + 1) % BUFFER_SIZE; // 다음 인덱스로 이동
+    }
+
+    return 0;
+}
